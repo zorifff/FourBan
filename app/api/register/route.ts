04b527151a80f35c688baa00e8d557957582eedf
Zhorif <zhorifzaidaan@gmail.com>
2026-05-08 18:08:08 +0700

    feat: implement NextAuth authentication with registration and login API routes

---
+++ app/api/register/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: Request) {
+  try {
+    // Tangkap username dari request
+    const { nama_lengkap, username, email, password } = await req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.tb_users.create({
+      data: {
+        nama_lengkap,
+        username, // Masukkan username ke database
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json({ message: "User berhasil dibuat", user });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Gagal membuat user" }, { status: 500 });
+  }
+}
